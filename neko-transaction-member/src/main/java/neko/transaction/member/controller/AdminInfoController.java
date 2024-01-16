0da50222d5a4c106b7c8e2d1fdc054a02e73242a
NEKO <2665249580@qq.com>
2024-01-16 14:13:30 +0800

    feat:(member.controller):管理员登录

--- neko-transaction-member/src/main/java/neko/transaction/member/controller/AdminInfoController.java
+++ neko-transaction-member/src/main/java/neko/transaction/member/controller/AdminInfoController.java
@@ -1,7 +1,17 @@
 package neko.transaction.member.controller;
 
+import neko.transaction.commonbase.utils.entity.ResultObject;
+import neko.transaction.member.service.AdminInfoService;
+import neko.transaction.member.vo.AdminInfoVo;
+import neko.transaction.member.vo.LogInVo;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * <p>
@@ -12,7 +22,19 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2024-01-15
  */
 @RestController
-@RequestMapping("/adminInfo")
+@RequestMapping("admin_info")
 public class AdminInfoController {
+    @Resource
+    private AdminInfoService adminInfoService;
 
+    /**
+     * 管理员登录
+     * @param vo 登录vo
+     * @param request HttpServletRequest
+     * @return 管理员信息vo
+     */
+    @PostMapping("login")
+    public ResultObject<AdminInfoVo> login(@Validated @RequestBody LogInVo vo, HttpServletRequest request){
+        return adminInfoService.login(vo, request);
+    }
 }
